1a3c11eec2b05aac42692524e67f0eb063ba3d55
Heidy Primo <heidyprimo10@hotmail.com>
2024-04-04 12:51:40 -0500

    adding  content to the Register component and styles

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,81 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import {
+    MDBBtn,
+  MDBContainer,
+  MDBInput,
+  MDBRow,
+  MDBCol
+  }
+  from 'mdb-react-ui-kit';
+  import "../styles/Register.css";
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <MDBContainer className="loginform">
+    <MDBRow className='box'>
+        <MDBCol col='4' md='7'>
+    
+            <h1 className='signUp'> Sign Up:</h1>
+        <form>
+          <MDBInput 
+          name="name"
+          
+          wrapperClass='mb-4' 
+          label='Name' 
+          id='form1'  
+          type='text'
+          />
+          <MDBInput 
+           name="username"
+          
+          wrapperClass='mb-4' 
+          label='Username' 
+          id='form1' 
+          type='text'
+          /> 
+          <MDBInput 
+           name="email"
+          
+          wrapperClass='mb-4' 
+          label='Email' 
+          id='form1' 
+          type='email'
+          />
+          <MDBInput
+           name="password"
+           
+          wrapperClass='mb-4' 
+          label='Password' 
+          id='form1' 
+          type='password'/>
+
+        <MDBInput
+           name="confirmPassword"
+          
+          wrapperClass='mb-4' 
+          label='Confirm Password' 
+          id='form1' 
+          type='password'
+          />
+
+          {/* <div className='d-flex justify-content-center mb-4'>
+            <MDBCheckbox name='flexCheck' id='flexCheckDefault' label='I have read and agree to the terms' />
+          </div> */}
+
+          <MDBBtn className="mb-4 w-100">Sign up</MDBBtn>
+
+          <p className='text-center text-muted mt-5 mb-0'>
+            Have already an account
+            <Link to="/login" className='fw-bold text-body'>
+              Login Here
+            </Link>
+          </p>
+        </form>
+    </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+
   )
 }
 
